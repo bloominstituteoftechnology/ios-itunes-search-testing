abc5c4141d66799a2c5e72b93c5ca187e664ee4a
Ezra Black <dev.ezrablack@gmail.com>
2020-04-20 11:24:36 -0500

    Added a Unit Test.

--- iTunes SearchTests/iTunes_SearchTests.swift
+++ iTunes SearchTests/iTunes_SearchTests.swift
@@ -7,26 +7,30 @@
 //
 
 import XCTest
+@testable import iTunes_Search
+/*
+ 
+ Does decoding work?
+ Does decoding fail when given bad data?
+ Does it build the correct URL?
+ Does it build the correct URLRequest?
+ are the search results saved properly?
+ Is the completion handler called when data is good?
+ Is the completion handler called when data is bad?
+ Is the completion handler called when the network fails?
+ 
+ 
+ */
 
 class iTunes_SearchTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+//runs perform search method
+    
+    func testForSomeResults() {
+        let controller = SearchResultController()
+        
+        controller.performSearch(for: "GarageBand", resultType: .software) {
+            print("Returned Valid Results ⚠️")
+            XCTAssertGreaterThan(controller.searchResults.count, 0)
         }
     }
 
