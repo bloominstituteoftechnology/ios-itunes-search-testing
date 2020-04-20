339d77d9de162750f793a49a2fcb29d6649a4cde
Ezra Black <dev.ezrablack@gmail.com>
2020-04-20 12:23:15 -0500

    created mock data file

---
+++ iTunes SearchTests/MockDataLoading.swift
@@ -0,0 +1,9 @@
+//
+//  MockDataLoading.swift
+//  iTunes SearchTests
+//
+//  Created by Joseph Rogers on 4/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
